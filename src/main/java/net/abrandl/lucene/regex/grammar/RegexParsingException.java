a6c2bb25737bddfdcab7567b0504495b12d4d25d
Andreas Brandl <mail@andreas-brandl.de>
2013-08-04 16:37:41 +0200

    Remove TODOs from Exception class.

--- src/main/java/net/abrandl/lucene/regex/grammar/RegexParsingException.java
+++ src/main/java/net/abrandl/lucene/regex/grammar/RegexParsingException.java
@@ -5,28 +5,23 @@ public class RegexParsingException extends Exception {
 	private static final long serialVersionUID = 2438749667894366906L;
 
 	public RegexParsingException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public RegexParsingException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RegexParsingException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RegexParsingException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public RegexParsingException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
