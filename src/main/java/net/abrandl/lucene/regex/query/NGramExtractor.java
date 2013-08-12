758380b81f08a92cb520d38c00ce75caddae7a42
Andreas Brandl <mail@andreas-brandl.de>
2013-08-12 18:36:25 +0200

    Add extractor for ngrams.

---
+++ src/main/java/net/abrandl/lucene/regex/query/NGramExtractor.java
@@ -0,0 +1,53 @@
+package net.abrandl.lucene.regex.query;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import net.abrandl.lucene.regex.query.bool.And;
+import net.abrandl.lucene.regex.query.bool.Expression;
+import net.abrandl.lucene.regex.query.bool.Literal;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class NGramExtractor {
+
+	private final NGramTokenizer tokenizer;
+
+	public NGramExtractor(NGramTokenizer tokenizer) {
+		checkNotNull(tokenizer);
+		this.tokenizer = tokenizer;
+	}
+
+	public Expression ngrams(String string) {
+		checkNotNull(string);
+
+		Set<String> ngrams = tokenizer.tokenize(string);
+		if (ngrams.isEmpty()) {
+			return Expression.any();
+		} else if (ngrams.size() == 1) {
+			return new Literal(ngrams.iterator().next());
+		} else {
+			HashSet<Expression> literals = new HashSet<Expression>(ngrams.size());
+			for (String chars : ngrams) {
+				literals.add(new Literal(chars));
+			}
+			return new And(literals);
+		}
+	}
+
+	public Expression ngrams(StringSet set) {
+		checkNotNull(set);
+
+		Iterator<String> iterator = set.iterator();
+
+		Expression e = ngrams(iterator.next());
+
+		while (iterator.hasNext()) {
+			Expression next = ngrams(iterator.next());
+			e = e.or(next);
+		}
+
+		return e;
+	}
+
+}
---
+++ src/test/java/net/abrandl/lucene/regex/query/NGramExtractorTest.java
@@ -0,0 +1,64 @@
+package net.abrandl.lucene.regex.query;
+
+import net.abrandl.lucene.regex.query.bool.Expression;
+import net.abrandl.lucene.regex.query.bool.Or;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NGramExtractorTest {
+
+	private NGramExtractor extractor;
+
+	@Before
+	public void setup() {
+		// TODO: stub the tokenizer?
+		extractor = new NGramExtractor(new NGramTokenizer());
+	}
+
+	@Test
+	public void extractsTrigramsForString() {
+		assertExpression("foo bar", Expression.and("foo", "oo ", "o b", " ba", "bar"));
+	}
+
+	@Test
+	public void emptyStringEqualsANY() {
+		assertExpression("", Expression.any());
+	}
+
+	@Test
+	public void tooShortStringEqualsANY() {
+		assertExpression("a", Expression.any());
+	}
+
+	@Test
+	public void tooShortStringEqualsANY2() {
+		assertExpression("ab", Expression.any());
+	}
+
+	@Test
+	public void supportsStringSets() {
+		StringSet set = new StringSet("foo boo", "fooooob");
+		Expression result = extractor.ngrams(set);
+		Expression expected = new Or(Expression.and("foo", "oo ", "o b", " bo", "boo"), Expression.and("foo", "ooo",
+				"oob"));
+		assertThat(result, equalTo(expected));
+	}
+
+	@Test
+	public void withShortStringsTheExpressionShouldBeANY() {
+		StringSet set = new StringSet("fo", "fooooob");
+		Expression result = extractor.ngrams(set);
+		Expression expected = new Or(Expression.any(), Expression.and("foo", "ooo", "oob"));
+		assertThat(result, equalTo(expected));
+	}
+
+	private void assertExpression(String string, Expression expected) {
+		Expression result = extractor.ngrams(string);
+		assertThat(result, equalTo(expected));
+	}
+
+}
