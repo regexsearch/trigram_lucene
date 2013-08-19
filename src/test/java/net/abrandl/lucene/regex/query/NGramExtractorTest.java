c2e0f46ad2bdcf01bb64a844b04e81ddeae6b184
Andreas Brandl <mail@andreas-brandl.de>
2013-08-19 17:54:32 +0200

    Stub-out the NGramTokenizer.

--- src/test/java/net/abrandl/lucene/regex/query/NGramExtractorTest.java
+++ src/test/java/net/abrandl/lucene/regex/query/NGramExtractorTest.java
@@ -1,59 +1,91 @@
 package net.abrandl.lucene.regex.query;
 
+import java.util.Collections;
+
 import net.abrandl.lucene.regex.NGramTokenizer;
 import net.abrandl.lucene.regex.query.bool.Expression;
 import net.abrandl.lucene.regex.query.bool.Or;
+import net.abrandl.test.Helpers;
+
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import static org.junit.Assert.assertThat;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(MockitoJUnitRunner.class)
 public class NGramExtractorTest {
 
 	private NGramExtractor extractor;
 
+	@Mock
+	private NGramTokenizer tokenizer;
+
 	@Before
 	public void setup() {
-		// TODO: stub the tokenizer?
-		extractor = new NGramExtractor(new NGramTokenizer());
+		extractor = new NGramExtractor(tokenizer);
 	}
 
 	@Test
 	public void extractsTrigramsForString() {
-		assertExpression("foo bar", Expression.and("foo", "oo ", "o b", " ba", "bar"));
+		String input = "foo bar";
+		String[] trigrams = new String[] { "foo", "oo ", "o b", " ba", "bar" };
+
+		when(tokenizer.tokenize(input)).thenReturn(Helpers.asSet(trigrams));
+
+		assertExpression(input, Expression.and(trigrams));
 	}
 
 	@Test
 	public void emptyStringEqualsANY() {
+		when(tokenizer.tokenize("")).thenReturn(Collections.<String> emptySet());
 		assertExpression("", Expression.any());
 	}
 
 	@Test
 	public void tooShortStringEqualsANY() {
+		when(tokenizer.tokenize("a")).thenReturn(Collections.<String> emptySet());
 		assertExpression("a", Expression.any());
 	}
 
 	@Test
 	public void tooShortStringEqualsANY2() {
+		when(tokenizer.tokenize("ab")).thenReturn(Collections.<String> emptySet());
 		assertExpression("ab", Expression.any());
 	}
 
 	@Test
 	public void supportsStringSets() {
+		String[] set1 = new String[] { "foo", "oo ", "o b", " bo", "boo" };
+		String[] set2 = new String[] { "foo", "ooo", "oob" };
+
+		when(tokenizer.tokenize("foo boo")).thenReturn(Helpers.asSet(set1));
+		when(tokenizer.tokenize("fooooob")).thenReturn(Helpers.asSet(set2));
+
 		StringSet set = new StringSet("foo boo", "fooooob");
 		Expression result = extractor.ngrams(set);
-		Expression expected = new Or(Expression.and("foo", "oo ", "o b", " bo", "boo"), Expression.and("foo", "ooo",
-				"oob"));
+
+		Expression expected = new Or(Expression.and(set1), Expression.and(set2));
 		assertThat(result, equalTo(expected));
 	}
 
 	@Test
 	public void withShortStringsTheExpressionShouldBeANY() {
+		String[] set2 = new String[] { "foo", "ooo", "oob" };
+
+		when(tokenizer.tokenize("fo")).thenReturn(Collections.<String> emptySet());
+		when(tokenizer.tokenize("fooooob")).thenReturn(Helpers.asSet(set2));
+
 		StringSet set = new StringSet("fo", "fooooob");
 		Expression result = extractor.ngrams(set);
-		Expression expected = new Or(Expression.any(), Expression.and("foo", "ooo", "oob"));
+		Expression expected = new Or(Expression.any(), Expression.and(set2));
 		assertThat(result, equalTo(expected));
 	}
 
