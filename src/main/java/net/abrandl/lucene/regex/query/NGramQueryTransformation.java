d445fd808d8246a17c1f47a37f39478e53a3f1ba
Andreas Brandl <mail@andreas-brandl.de>
2013-09-01 16:02:27 +0200

    Restructure class.

--- src/main/java/net/abrandl/lucene/regex/query/NGramQueryTransformation.java
+++ src/main/java/net/abrandl/lucene/regex/query/NGramQueryTransformation.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import net.abrandl.lucene.regex.NGramTokenizer;
 import net.abrandl.lucene.regex.grammar.tree.RegexNode;
 import net.abrandl.lucene.regex.query.bool.Expression;
+import net.abrandl.lucene.regex.query.bool.NestedExpressionSimplifier;
 
 public class NGramQueryTransformation implements QueryTransformation {
 
@@ -20,18 +21,28 @@ public class NGramQueryTransformation implements QueryTransformation {
 
 	@Override
 	public Expression expressionFor(RegexNode regex) {
+		RegexAnalyzer analyzer = createAnalyzer();
 
+		// convert regex to ngram expression
+		Expression match = regex.accept(analyzer).getMatch();
+
+		// simplify nested expressions
+		return match.accept(new NestedExpressionSimplifier());
+
+	}
+
+	private RegexAnalyzer createAnalyzer() {
 		NGramTokenizer tokenizer = new NGramTokenizer(gramSize);
 		NGramExtractor extractor = new NGramExtractor(tokenizer);
 		RegexInfoTransformation prefixToMatch = new PrefixToMatchTransformation(extractor);
 		RegexInfoTransformation suffixToMatch = new SuffixToMatchTransformation(extractor);
 		RegexInfoTransformation exactToMatch = new ExactToMatchTransformation(extractor);
+		// RegexInfoTransformation matchSimplification = new
+		// MatchSimplificationTransformation();
 		RegexInfoTransformation transformations = new CompositeRegexInfoTransformation(Arrays.asList(exactToMatch,
-				suffixToMatch, prefixToMatch));
+				prefixToMatch, suffixToMatch));
 		RegexAnalyzer analyzer = new RegexAnalyzer(transformations);
-
-		return regex.accept(analyzer).getMatch();
-
+		return analyzer;
 	}
 
 }
