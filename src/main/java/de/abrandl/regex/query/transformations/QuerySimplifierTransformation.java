caf19c0eff12eadf6ad6f730e2e145adf92dc610
Andreas Brandl <mail@andreas-brandl.de>
2013-10-25 15:53:29 +0200

    Multi-pass query simplifier.

--- src/main/java/de/abrandl/regex/query/transformations/QuerySimplifierTransformation.java
+++ src/main/java/de/abrandl/regex/query/transformations/QuerySimplifierTransformation.java
@@ -1,19 +1,44 @@
 package de.abrandl.regex.query.transformations;
 
+import java.util.Arrays;
+import java.util.List;
+
 import de.abrandl.regex.query.RegexInfo;
 import de.abrandl.regex.query.RegexInfoTransformation;
-import de.abrandl.regex.query.bool.Expression;
-import de.abrandl.regex.query.bool.ExpressionFlattener;
-import de.abrandl.regex.query.bool.ExpressionSimplifier;
+import de.abrandl.regex.query.bool.*;
 
 public class QuerySimplifierTransformation implements RegexInfoTransformation {
 
+	private final static int MAX_PASSES = 10;
+
+	private final List<ExpressionVisitor<Expression>> rules;
+
+	public QuerySimplifierTransformation() {
+		rules = Arrays.asList(new ExpressionSimplifier(), new ExpressionFlattener(), new AbsorptionSimplifier());
+	}
+
 	@Override
 	public RegexInfo transform(RegexInfo result) {
-		Expression match = result.getMatch();
-		match = match.accept(new ExpressionSimplifier());
-		match = match.accept(new ExpressionFlattener());
-		return replaceMatch(result, match);
+		Expression simplified = simplify(result.getMatch());
+		return replaceMatch(result, simplified);
+	}
+
+	private Expression simplify(Expression input) {
+
+		Expression current = input;
+		Expression before = null;
+
+		int pass = 0;
+
+		while (!(current.equals(before)) && pass < MAX_PASSES) {
+			before = current;
+			for (ExpressionVisitor<Expression> rule : rules) {
+				current = current.accept(rule);
+			}
+			pass++;
+		}
+
+		return current;
 	}
 
 	private RegexInfo replaceMatch(RegexInfo result, Expression match) {
